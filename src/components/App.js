74428cd12b215e076bbfed76cd770003eae9bc74
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2020-04-21 15:13:22 +0100

    The implementation of the basic components(i.e App, Story and Stories) required to get the project running has been completed for now. This implementations are temporary and likely to chage

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './App.css';
+import Stories from './Stories';
+const App = ({ stories }) =>
+  <div className="app">
+    <Stories stories={stories} />
+  </div>
+export default App;
